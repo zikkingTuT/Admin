9a2299435509be1219aa6540bac398d584439a82
zikkingTuT <1219367833@qq.com>
2023-05-23 11:10:43 +0800

    新建测试项目

---
+++ src/main/java/com/example/testadmindemo/TestAdminDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testadmindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAdminDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAdminDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testadmindemo/TestAdminDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testadmindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAdminDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
