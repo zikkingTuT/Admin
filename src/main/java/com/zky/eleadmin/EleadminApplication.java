dec7233dab89be0edca4ee17d4c49fb96f978b2f
zhangkaiyue <1219367833@qq.com>
2025-04-10 19:54:39 +0800

    创建 eladmin 项目

---
+++ src/main/java/com/zky/eleadmin/EleadminApplication.java
@@ -0,0 +1,13 @@
+package com.zky.eleadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EleadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zky/eleadmin/EleadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zky.eleadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
