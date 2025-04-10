73f447042907097e1a72c14b81540dc40b83a7f2
zhangkaiyue <1219367833@qq.com>
2025-04-10 20:08:40 +0800

    修改 user 输出逻辑

--- src/main/java/com/zky/eleadmin/controller/UserController.java
+++ src/main/java/com/zky/eleadmin/controller/UserController.java
@@ -2,10 +2,10 @@ package com.zky.eleadmin.controller;
 
 public class UserController {
     public UserController() {
-        System.out.printf("111");
-        System.out.printf("111");
-        System.out.printf("111");
-        System.out.printf("111");
-        System.out.printf("111");
+        System.out.print("111");
+        System.out.print("111");
+        System.out.print("111");
+        System.out.print("111");
+        System.out.print("111");
     }
 }
