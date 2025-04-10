aa3b39edbd8f3dccae67ea7bb5a2899287ecf028
zhangkaiyue <1219367833@qq.com>
2025-04-10 20:06:11 +0800

    创建 user 逻辑

--- src/main/java/com/zky/eleadmin/controller/UserController.java
+++ src/main/java/com/zky/eleadmin/controller/UserController.java
@@ -3,5 +3,9 @@ package com.zky.eleadmin.controller;
 public class UserController {
     public UserController() {
         System.out.printf("111");
+        System.out.printf("111");
+        System.out.printf("111");
+        System.out.printf("111");
+        System.out.printf("111");
     }
 }
